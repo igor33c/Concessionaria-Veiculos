65575ce9d37e03b75010df1936c4e37cd7fda0fa
igor33c <108588088+igor33c@users.noreply.github.com>
2023-04-06 23:50:08 -0300

    #09 assistencia tecnica page

--- src/Components/FullPages/Assistencia.jsx
+++ src/Components/FullPages/Assistencia.jsx
@@ -1,15 +1,94 @@
 import MenuModelos from "../../Components/MenuModelos";
 import TopInfo from "../../Components/TopInfo";
 import TopMenu from "../../Components/TopMenu";
+import { useState } from "react";
 
-function Assistencia (){
+function Assistencia() {
+    const [phone, setPhone] = useState('');
+  
+    function formatPhone(value) {
+      // remover nao numericos
+      const phone = value.replace(/\D/g, '');
+  
+      // verifica se ta vazio ou nao
+
+      if (phone.length === 0) {
+        return '';
+      }
+  
+      // formatando para formato cel
+      let formattedPhone = phone.substring(0, 11);
+      formattedPhone = 
+      `(${formattedPhone.substring(0, 2)})${formattedPhone.substring(2, 7)}-${formattedPhone.substring(7)}`;
+      
+      return formattedPhone;
+    }
+  
+    function handleChange(e) {
+      const formattedPhone = formatPhone(e.target.value);
+      setPhone(formattedPhone);
+}
     return(
         <div>
             <TopMenu />
             <TopInfo />
             <MenuModelos />            
-            <div className="px-4">
-                <h1>Assistencia Técnica</h1>    
+            <div className="px-4 py-8 bg-gradient-to-br from-green-50 to-blue-100 ">
+                <h1 className="justif-center items-center text-center text-blue-500 font-bold text-5xl">
+                    Assistencia Técnica
+                </h1>
+                <div className="px-4 py-8">
+                    <div className="pt-8">
+                        <h2 className="text-red-500 font-bold text-2xl">
+                            Preencha o Formulário abaixo:
+                        </h2>
+                    </div>
+                    <div>
+                        <form>
+                            <div className="py-1">
+                                <label >
+                                    Seu Nome:
+                                    <div>
+                                        <input
+                                            name="isGoing"
+                                            type="text"
+                                        />
+                                    </div>
+                                </label>
+                            </div>
+                            <div className="py-1 flex">   
+                                <label 
+                                    for="email">
+                                    Seu e-mail:
+                                    <div className="">
+                                        <input                                    
+                                            type="email"
+                                            id="email"
+                                            name="email"
+                                        />
+                                    </div> 
+                                </label>
+                            </div> 
+                            <div className="py-1">
+                                <label for="tel">
+                                    Seu Celular:
+                                    <div>
+                                        <input
+                                            className="px-1" 
+                                            type="tel" 
+                                            id="phone" 
+                                            name="phone" 
+                                            value={phone} 
+                                            onChange={handleChange} 
+                                            pattern="\([0-9]{2}\) [0-9]{5}-[0-9]{4}" 
+                                            required 
+                                        />
+                                    </div>
+                                </label>
+                            </div>
+                        </form>
+                    </div>  
+                </div>
             </div>    
         </div>
     )    
--- src/Components/Vehicles/AllVehicles.jsx
+++ src/Components/Vehicles/AllVehicles.jsx
@@ -12,28 +12,42 @@ function AllVehicles (){
             <TopMenu />
             <TopInfo />
             <MenuModelos />
-            <div className="bg-gradient-to-br from-blue-200 to-violet-300 px-4 car grid grid-cols-1 
+            <div className="bg-gradient-to-br from-blue-200 to-violet-300 px-8 car grid grid-cols-1 
                             sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 
                             xl:grid-cols-4 gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12 py-24"
             >
-                {Object.keys(allData).map((carId) => (/// mapeando o obj usando carId como Key
-                    <div key={carId} className="car mr-4 ml-4"> 
-                    {
-                    ///div para cada veiculo ser mostrado separadamente
-                    }
-                        <h2>{allData[carId].marca} {allData[carId].modelo}</h2> 
-                        {
-                            //mostrar dados
-                        }
-                        <p>Ano: {allData[carId].ano}</p>
-                        <p>Kilometragem: {allData[carId].km}</p>
-                        <p>Preço: R${allData[carId].preco.toFixed(2)}</p>
-                        <Link to="/comprar" state={allData[carId]}> 
-                        <img src={allData[carId].photo_1}  alt="" className="w-48 h-32"/>
-                        </Link>                                            
-                    </div>
-                    
-                ))}
+                {
+                    Object.keys(allData).map(
+                        (carId) => 
+                            (/// mapeando o obj usando carId como Key
+                                <div key={carId} className="w-full md:w-auto px-2"
+                                > 
+                                {
+                                ///div para cada veiculo ser mostrado separadamente
+                                }
+                                    <Link to="/comprar" state={allData[carId]}> 
+                                        <img src={allData[carId].photo_1}  alt="" className="w-full md:w-64 h-48  
+                                                                                             object-cover"
+                                        />
+                                    </Link> 
+                                    <div className=" p-2 text-center">
+                                        <p className="font-bold text-lg">
+                                            {allData[carId].marca} {allData[carId].modelo} 
+                                        </p>
+                                        <p>
+                                        Ano: {allData[carId].ano}
+                                        </p>
+                                        <p>
+                                            Kilometragem: {allData[carId].km}
+                                        </p>
+                                        <p className="font-bold text-lg text-blue-500">
+                                            Preço: R${allData[carId].preco.toFixed(2)}
+                                        </p>
+                                    </div>                                    
+                                </div>                                    
+                        )
+                    )
+                }
             </div>
         </div>
     )
